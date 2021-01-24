97c3857933b1d60f7c6ef1e783d6ea5b57e87dbb
Hao <liu.hao@hotmail.com>
2021-01-24 11:04:27 -0600

    implement the function of User login

---
+++ book/src/main/java/com/nwavguy/web/LoginServlet.java
@@ -0,0 +1,32 @@
+package com.nwavguy.web;
+
+import com.nwavguy.pojo.User;
+import com.nwavguy.service.UserService;
+import com.nwavguy.service.impl.UserServiceImpl;
+
+import javax.servlet.ServletException;
+import javax.servlet.http.HttpServlet;
+import javax.servlet.http.HttpServletRequest;
+import javax.servlet.http.HttpServletResponse;
+import java.io.IOException;
+
+public class LoginServlet extends HttpServlet {
+    private UserService userService = new UserServiceImpl();
+
+    @Override
+    protected void doPost(HttpServletRequest req, HttpServletResponse resp) throws ServletException, IOException {
+        // get parameters from request
+        String username = req.getParameter("username");
+        String password = req.getParameter("password");
+
+        // call user service login method
+        User user = userService.login(new User(null, username, password, null));
+        if (user == null) {
+            System.out.println("login failed");
+            req.getRequestDispatcher("/pages/user/login.html").forward(req, resp);
+        } else {
+            System.out.println("login success");
+            req.getRequestDispatcher("/pages/user/login_success.html").forward(req, resp);
+        }
+    }
+}
